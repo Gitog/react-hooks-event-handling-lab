33ffe91a2bac398fe36e00461a0f53ca532ac58d
James <jamesnjorio@gmail.com>
2022-09-29 17:48:12 +0300

    Lab complete

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,21 @@
 // Code EyesOnMe Component Here
+import React, {useState} from 'react';
+
+function EyesOnMe(){
+
+    // function checkFocus(){
+    //     onFocus={(e) => {
+    //         console.log('Good'); 
+    // }
+
+    return(
+        <div>
+            <button 
+            onFocus={()=>console.log('Good!')}
+            onBlur={()=>console.log('Hey! Eyes on me!')}
+            >Eyes on me</button>
+        </div>
+    )
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,19 @@
 // Code Keypad Component Here
+import React from 'react';
+
 
 function Keypad (){
+
+    function handleChnage(){
+       
+        console.log("Entering password...")
+    }
     return (
-        <div></div>
+        <div>
+            <keypad>
+                <input type="password" onChange={handleChnage}/>
+            </keypad>
+        </div>
     )
 }
 
